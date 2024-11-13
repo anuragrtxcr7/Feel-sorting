952463b966879bbccef5866752517e3e576e206c
Anurag Jain <jain.anurag0203@gmail.com>
2024-11-13 14:52:34 +0530

    updated bubble sort's waitform

--- sorting_algos/insertion.js
+++ sorting_algos/insertion.js
@@ -1,39 +1,31 @@
-async function insertion(){
-    console.log('In insertion()');
+async function insertion() {
+    console.log("In insertion()");
     const ele = document.querySelectorAll(".bar");
     // color
-    ele[0].style.background = 'green';
-    for(let i = 1; i < ele.length; i++){
-        console.log('In ith loop');
+    ele[0].style.background = "green";
+    for (let i = 1; i < ele.length; i++) {
+        console.log("In ith loop");
         let j = i - 1;
         let key = ele[i].style.height;
         // color
-        ele[i].style.background = 'blue';
+        ele[i].style.background = "blue";
 
-        await waitforme(delay);
-
-        while(j >= 0 && (parseInt(ele[j].style.height) > parseInt(key))){
-            console.log('In while loop');
+        while (j >= 0 && parseInt(ele[j].style.height) > parseInt(key)) {
+            console.log("In while loop");
             // color
-            ele[j].style.background = 'blue';
+            await waitforme(delay);
+            ele[j].style.background = "blue";
+            ele[j + 1].style.background = "green";
             ele[j + 1].style.height = ele[j].style.height;
             j--;
-
-            await waitforme(delay);
-
-            // color
-            for(let k = i; k >= 0; k--){
-                ele[k].style.background = 'green';
-            }
         }
         ele[j + 1].style.height = key;
-        // color
-        ele[i].style.background = 'green';
+        ele[j + 1].style.background = "green";
     }
 }
 
 const inSortbtn = document.querySelector(".insertionSort");
-inSortbtn.addEventListener('click', async function(){
+inSortbtn.addEventListener("click", async function () {
     disableSortingBtn();
     disableSizeSlider();
     disableNewArrayBtn();
@@ -42,5 +34,3 @@ inSortbtn.addEventListener('click', async function(){
     enableSizeSlider();
     enableNewArrayBtn();
 });
-
-
